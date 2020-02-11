de76540ffff55f991e1398c8bea0c14662947f13
Aaron Hoskins <tobyhoskinsdev@gmail.com>
2020-02-11 12:07:26 -0500

    Initial commit

---
+++ app/src/main/java/com/examples/coding/fragmentsdemo/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.examples.coding.fragmentsdemo
+
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity(), OnFragmentInteractionListener,
+    BlueFragment.OnFragmentInteractionListener,
+    GreenFragment.OnFragmentInteractionListener{
+    val redFragment : RedFragment by lazy { supportFragmentManager.findFragmentById(R.id.fgRedFragment) as RedFragment }
+    val blueFragment by lazy{BlueFragment()}
+    val greenFragment by lazy{GreenFragment.newInstance(blueValue, redValue)}
+    lateinit var redValue : String
+    lateinit var blueValue : String
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        redFragment.setFragListener(this)
+
+        //To Add fragment Dynamically
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.frmFragmentDisplayOne, blueFragment)
+            .addToBackStack("BLUE_FRAG")
+            .commit()
+    }
+
+    override fun dataFromRedFragment(value: String) {
+        Toast.makeText(this, value, Toast.LENGTH_LONG).show()
+        redValue = value
+        displayAllValues()
+    }
+
+    override fun onFragmentInteraction(string : String) {
+        Toast.makeText(this, string, Toast.LENGTH_LONG).show()
+        blueValue = string
+        displayAllValues()
+    }
+
+    fun displayAllValues() {
+        if(this::redValue.isInitialized && this::blueValue.isInitialized) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.frmFragmentDisplayTwo, greenFragment)
+                .addToBackStack("GREEN_FRAG")
+                .commit()
+        }
+    }
+
+    override fun onFragmentInteraction() {
+    }
+
+
+}
